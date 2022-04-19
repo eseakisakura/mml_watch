0b3c3b1d087f9d8667ab86baee7406ebbbbbf483
eseakisakura <bug.jamming@aol.com>
2022-04-19 14:37:22 +0900

    wintest2

---
+++ mml_watch_gui/alpha/Watch_alpha.cmd
@@ -0,0 +1,13 @@
+@rem Watch_alpha.cmd 
+@echo off
+cd /d %~dp0
+
+echo "Powershell"を起動します
+echo.
+
+cd .\script
+
+rem $Env:Path+= ";pwsh.exeへのフォルダ"
+
+rem // -Sta シングルスレッド指定、PS2.0以前必須 //
+pwsh.exe -ExecutionPolicy RemoteSigned -Sta -File .\common.ps1 "mml_watch"
